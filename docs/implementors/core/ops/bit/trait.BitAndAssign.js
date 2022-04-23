54432ef9668c4e3649c2e49203bc0006bc927f5e
David Taralla <davidtaralla@gmail.com>
2022-04-23 13:35:17 -0400

    Update docs

---
+++ docs/implementors/core/ops/bit/trait.BitAndAssign.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["rrise"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;<a class=\"struct\" href=\"rrise/struct.AkCallbackType.html\" title=\"struct rrise::AkCallbackType\">AkCallbackType</a>&gt; for <a class=\"struct\" href=\"rrise/struct.AkCallbackType.html\" title=\"struct rrise::AkCallbackType\">AkCallbackType</a>","synthetic":false,"types":["rrise::bindings::root::AkCallbackType"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

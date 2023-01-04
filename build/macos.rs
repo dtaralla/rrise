c0311d02b892072920496447fe1190f9a0a97705
Sahin Habermann <sahyno1996@gmail.com>
2023-01-04 16:46:48 +0100

    Remove UNIX include from build/macos and cleaned up build.rs

--- build/macos.rs
+++ build/macos.rs
@@ -2,8 +2,6 @@
  * Copyright (c) 2022 Contributors to the Rrise project
  */
 
-include!("unix.rs");
-
 /// Updates build environment with required dependencies for MacOS targets
 fn platform_dependencies(wwise_sdk: &PathBuf, config_folder: &str) {
     //  Add needed MacOS frameworks
@@ -36,4 +34,31 @@ fn platform_dependencies(wwise_sdk: &PathBuf, config_folder: &str) {
     println!("cargo:rustc-link-lib=framework=AppKit");
     println!("cargo:rustc-link-lib=framework=Cocoa");
     println!("cargo:rustc-link-lib=framework=CoreFoundation");
+}
+
+/// Updates the default stream manager cc build specs for MacOS targets
+fn stream_cc_platform_specifics(build: &mut cc::Build, wwise_sdk: &PathBuf) -> io::Result<()> {
+    build
+        .compiler("clang")
+        .shared_flag(true)
+        .opt_level(2)
+        .flag("-std=c++17")
+        .flag("-MMD")
+        .flag("-MP")
+        .flag("-fPIC")
+        .flag("-g")
+        .flag("-Wno-invalid-offsetof")
+        .flag("-fno-exceptions")
+        .flag("-fno-rtti")
+        .define("AUDIOKINETIC", None)
+        .file(
+            wwise_sdk
+                .join("samples")
+                .join("SoundEngine")
+                .join("POSIX")
+                .join("AkDefaultIOHookBlocking.cpp"),
+        )
+        .include(wwise_sdk.join("samples").join("SoundEngine").join("POSIX"));
+
+    Ok(())
 }
